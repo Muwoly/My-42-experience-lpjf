59e2d253f3d74c3b0cc200974f67b3288e9da7b2
loris <loris.penelon@outlook.com>
2022-03-16 16:49:49 +0100

    20

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,65 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpenelon <lpenelon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 18:01:13 by lpenelon          #+#    #+#             */
-/*   Updated: 2022/03/15 19:22:26 by lpenelon         ###   ########.fr       */
+/*   Updated: 2022/03/16 16:47:52 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_to_trim(char const *s1, char const *set)
+static size_t	flag(char c, char const *set)
 {
 	size_t	i;
-	size_t	j;
-	size_t	ret;
 
 	i = 0;
-	ret = 0;
-	while (i < ft_strlen(s1))
+	while (set[i])
 	{
-		j = 0;
-		while (j < ft_strlen(set))
-		{
-			if (s1[i] == set[j])
-				ret++;
-			j++;
-		}
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	return (ret);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*ret;
-	char	tmp[ft_strlen(s1) +1];
+	size_t	start;
+	size_t	end;
 	size_t	i;
-	size_t	j;
 
-	if (!s1 || !set)
-		return (0);
-	ret = (char *) malloc(ft_strlen(s1) - count_to_trim(s1, set) + 1);
+	start = 0;
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	while (s1[start] && flag(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && flag(s1[end - 1], set))
+		end--;
+	ret = (char *) malloc(sizeof(*s1) * (end - start + 1));
 	if (ret == NULL)
 		return (NULL);
-	ft_strlcpy(tmp, s1, (ft_strlen(s1) + 1));
 	i = 0;
-	while (i < ft_strlen(s1))
+	while (start < end)
 	{
-		j = 0;
-		while (j < ft_strlen(set))
-		{
-			if (tmp[i] == set[j])
-			{
-				ft_strlcpy(&tmp[i], &tmp[i + 1], ft_strlen(tmp) - i + 1);
-				j = -1;
-			}
-			j++;
-		}
+		ret[i] = s1[start];
 		i++;
+		start++;
 	}
-	ft_strlcpy(ret, tmp, (ft_strlen(s1) - count_to_trim(s1, set) + 1));
+	ret[i] = '\0';
 	return (ret);
 }
