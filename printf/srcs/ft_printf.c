af70a71ad81a16dc9fd75d81872a5a05e89321e1
loris <you@example.com>
2022-04-07 22:10:43 +0100

    42

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -12,22 +12,37 @@
 
 #include "ft_printf.h"
 
+void	dispatch(const char s, ...)
+{
+	if (s == c)
+		ft_putchar_fd(c, 1);
+}
+
 int	ft_printf(const char *s, ...)
 {
 	int	i;
+	int	ret;
+	va_list args;
+
+// https://youtu.be/S-ak715zIIE?t=185
 
 	i = 0;
+	ret = 0;
+	va_start(args, s);
+	vsnprintf(stderr, format, args);
 	while (s[i])
 	{
 		if (s[i] == '%')
 		{
 			i++;
+			dispatch(s[i], ...)
 		}
 		else
 			ft_putchar_fd(s[i], 1);
 		i++;
 	}
-	return (1);
+	return (i + 1);
+	va_end(argptr);
 }
 
 // has to include cspdiuxX%
